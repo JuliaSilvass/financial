d8ead833daece00ed7ace06faed0dd056ee80da6
Julia Silva <juliasilvass01@gmail.com>
2025-09-26 19:50:22 -0300

    feat: conexao com o banco de dados funcionando

--- database/connection_db.py
+++ database/connection_db.py
@@ -1,11 +1,39 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, declarative_base 
 from dotenv import load_dotenv
 import os
 
 #carrega as variaveis de ambiente do arquivo .env
 load_dotenv()
 
-# Configurações do banco de dados
-DATABASE_URL = "postgresql+psycopg2://user:password@localhost:port/financial_db"
+# carrega as variaveis de ambiente do arquivo .env
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
+# Configurações do banco de dados
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Criar engine (responsável pela conexão)
+engine = create_engine(DATABASE_URL, echo=True)
+
+# Criar sessão (responsável pelas operações no banco)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base para os modelos (tabelas)
+Base = declarative_base()
+
+
+
+def test_connection():
+    try:
+        # Tenta conectar ao banco de dados
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT version();"))
+            version = result.fetchone()
+            print("Conexão bem-sucedida ao banco de dados!")
+            print(f"Versão do banco de dados: {version[0]}")
+    except Exception as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-from database.connection import connect_to_database
+from database.connection_db import test_connection
 from controllers.user_controller import UserController
 from utils.security import hash_password
 from models.user import User
 
-conn = connect_to_database()
-controller = UserController(conn)
 
-# criação de usuário
-new_user = User(name="Julia4", email="julia4@gmail.com", password=hash_password("123456"))
-controller.create_user(new_user)
-
-# consulta de usuário por email
-# user = controller.get_user_by_email("julia@gmail.com")
-
-
-
-conn.close()
+if __name__ == "__main__":
+    test_connection()
