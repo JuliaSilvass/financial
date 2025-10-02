42ee33d681bdcaec3fb51de489480c6e46b5de80
Julia Silva <juliasilvass01@gmail.com>
2025-10-02 20:21:16 -0300

    feat: Criado tela home

--- ui/home_page.py
+++ ui/home_page.py
@@ -9,18 +9,47 @@ def home_page(page: ft.Page):
     def register_click(e):
         print("Clicou em registrar")
 
-    btn_login = ft.ElevatedButton("Login", on_click=login_click)
-    btn_register = ft.ElevatedButton("Registrar", on_click=register_click)
-    
-    page.add(
-        ft.Row(
-            [
-                ft.Text("Bem-vindo ao financial!", size=40, weight="bold", color="Green"),
-                ft.Text("Aqui você pode gerenciar sua vida financeira, e acompanhar o andamento de suas finanças.", size=16),
-                ft.Text("Para começar, faça login ou registre-se.", size=16),
-            ],
-            alignment="center"
+        # Botão de Login (principal)
+    btn_login = ft.ElevatedButton(
+        text="Login",
+        icon=ft.Icons.LOGIN,
+        style=ft.ButtonStyle(
+            bgcolor={"":"#44CFA1"},
+            color={"":"white"},
+            padding=20,
+            shape=ft.RoundedRectangleBorder(radius=10),
         ),
-        btn_login,
-        btn_register
+        on_click=login_click
+    )
+
+    # Botão de Registrar (alternativo)
+    btn_register = ft.ElevatedButton(
+        text="Registrar",
+        icon=ft.Icons.APP_REGISTRATION,
+        style=ft.ButtonStyle(
+            bgcolor={"":"#44CFA1"},
+            color={"":"white"},
+            padding=20,
+            shape=ft.RoundedRectangleBorder(radius=10),
+            side={"":ft.BorderSide(2, "#44CFA1")} 
+        ),
+        on_click=register_click
+    )
+
+    page.add(
+        ft.Column(
+            controls=[
+                ft.Text("Bem vindo ao Financial!", theme_style="displayMedium", size=50, weight="bold", color="#44CFA1", no_wrap=False),
+                ft.Text("Aqui você pode gerenciar sua vida financeira e acompanhar o andamento de suas finanças.", size=25, no_wrap=False),
+                ft.Divider(),
+                ft.Text("Para começar, faça login ou registre-se.", size=25),
+                ft.Row(
+                    controls=[btn_login, btn_register],
+                    alignment="center"
+                )
+            ],
+            alignment="center",
+            horizontal_alignment="center",
+            expand=True
+        )
     )
